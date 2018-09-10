0077dc3f2235cbafa251348840a464fc9e0f7b69
Vo <vo@Vos-iMac.local>
2018-09-10 10:25:22 +1000

    Modified firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,11 +2,11 @@ import * as firebase from 'firebase';
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "expensify-455b4.firebaseapp.com",
-    databaseURL: "https://expensify-455b4.firebaseio.com",
-    projectId: "expensify-455b4",
-    storageBucket: "expensify-455b4.appspot.com",
-    messagingSenderId: "1029316834532"
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
   firebase.initializeApp(config);
